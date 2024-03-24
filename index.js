d38fe167e6a3a055cb08636d9ed59859ebd75fc7
ArnoldMajor <arnoldmwangakala@gmail.com>
2024-03-24 13:53:06 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+beforeEach (function(){
+    cats.length = 0;
+    cats.push('Milo', 'Otis', 'Garfield');
+});
+
+function destructivelyAppendCat(){
+    cats.push('Ralph')
+    return cats;    
+}
+
+function destructivelyPrependCat(){
+    cats.unshift('Bob');
+    return cats;
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+    return cats
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+    return cats
+}
+
+function appendCat(){
+    const catsNew = [...cats, 'Broom'];
+    return catsNew;
+}
+
+function prependCat(){
+    const catsNew = ['Arnold', ...cats];
+    return catsNew;
+}
+
+function removeLastCat(){
+    const catsNew = cats.slice(0,2);
+    return catsNew; 
+}
+
+function removeFirstCat(){
+    const catsNew = cats.slice(1,3);
+    return catsNew;
+}
